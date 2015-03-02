13904ccac8c0eac64401d7aa3ee9e68417d44277
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-03-01 23:09:05 -0300

    Created test suite to CnpjValidator
    - setUp
    - tearDown
    - testValidCnpj
    - testEmptyCnpj
    - testeMinorLengthCnpj

---
+++ tests/Validations/CnpjValidatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Websix\BrValidations\Validations;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+class CpnjValidatorTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        $this->cnpjValidator = new CnpjValidator();
+    }
+
+    public function tearDown()
+    {
+        unset($this->cnpjValidator);
+    }
+
+    public function testValidCnpj()
+    {
+        $this->assertTrue($this->cnpjValidator->validate('00776574000660'));
+    }
+
+    /**
+     * @expectedException \Websix\BrValidations\Exceptions\EmptyArgumentException
+     */
+    public function testEmptyCnpj()
+    {
+        $this->assertTrue($this->cnpjValidator->validate(''));
+    }
+
+    /**
+     * @expectedException \Websix\BrValidations\Exceptions\InvalidLengthException
+     */
+    public function testMinorLength()
+    {
+        $this->assertTrue($this->cnpjValidator->validate('0077657400066'));
+    }
+
+}
