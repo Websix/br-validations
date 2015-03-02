658e1b17eae783dee6d986f1dcff5834210f5399
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-03-02 01:15:35 -0300

    Added tests to major string length and not only digits

--- tests/Validations/CnpjValidatorTest.php
+++ tests/Validations/CnpjValidatorTest.php
@@ -34,4 +34,20 @@ class CpnjValidatorTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($this->cnpjValidator->validate('0077657400066'));
     }
 
+    /**
+     * @expectedException \Websix\BrValidations\Exceptions\InvalidLengthException
+     */
+    public function testMajorLength()
+    {
+        $this->assertTrue($this->cnpjValidator->validate('007765740006600'));
+    }
+
+    /**
+     * @expectedException \Websix\BrValidations\Exceptions\NotOnlyDigitsException
+     */
+    public function testFailNotOnlyDigits()
+    {
+        $this->assertTrue($this->cnpjValidator->validate('00.776.574/0006-60'));
+    }
+
 }
